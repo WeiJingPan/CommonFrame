b0663c3ede1badd8bd62946a33ca72a28600cd22
WeiJingPan <466676342@qq.com>
2017-06-22 03:29:45 +0800

    Frame-1

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class Client : MonoBehaviour
+{
+    private Socket client;
+    public byte[] byteData=new byte[1024];
+
+    void Start()
+    {
+        Initail();
+    }
+
+    void Initail()
+    {
+        IPAddress ipAddress=IPAddress.Parse("127.0.0.1");
+        IPEndPoint ipEnd=new IPEndPoint(ipAddress,18010);
+        client=new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+        client.BeginConnect(ipEnd,ConnectCallBack,client);
+    }
+
+    public void ConnectCallBack(IAsyncResult ar)
+    {
+        client.EndReceive(ar);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            BeginSend("12345678");
+        }
+        BeginRecv();
+    }
+
+    #region Receive
+
+    public void BeginRecv()
+    {
+        client.BeginReceive(byteData, 0, 1024, SocketFlags.None, RecvCallBack, this);
+
+    }
+
+    private void RecvCallBack(IAsyncResult ar)
+    {
+        int length = client.EndReceive(ar);
+        string tmpStr = Encoding.Default.GetString(byteData, 0, length);
+        Debug.Log("server recv ==" + tmpStr);
+        BeginSend(tmpStr);
+    }
+    #endregion
+
+    #region Send
+    public void BeginSend(string tmpStr)
+    {
+        byte[] data = Encoding.Default.GetBytes(tmpStr);
+        client.BeginSend(data, 0, data.Length, SocketFlags.None, SendCallBack, this);
+    }
+
+    private void SendCallBack(IAsyncResult ar)
+    {
+        int byteSend = client.EndSend(ar);
+        Debug.Log("byte send count ==" + byteSend);
+    }
+
+    #endregion
+    
+}
---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+public class SocketState
+{
+    public Socket socket;
+    public byte[] buffer;
+
+    public SocketState(Socket tmpSocket)
+    {
+        buffer=new byte[1024];
+        socket = tmpSocket;
+    }
+    #region Receive
+
+    public void BeginRecv()
+    {
+        socket.BeginReceive(buffer, 0, 1024, SocketFlags.None, RecvCallBack, this);
+
+    }
+
+    private void RecvCallBack(IAsyncResult ar)
+    {
+        int length = socket.EndReceive(ar);
+        string tmpStr = Encoding.Default.GetString(buffer, 0, length);
+        Debug.Log("server recv ==" + tmpStr);
+        BeginSend(tmpStr);
+    } 
+    #endregion
+    #region Send
+    
+    #endregion
+    public void BeginSend(string tmpStr)
+    {
+        byte[] data = Encoding.Default.GetBytes(tmpStr);
+        socket.BeginSend(data, 0, data.Length, SocketFlags.None, SendCallBack, this);
+    }
+
+    private void SendCallBack(IAsyncResult ar)
+    {
+        int byteSend = socket.EndSend(ar);
+        Debug.Log("byte send count =="+byteSend);
+    }
+}
+
+public class Server : MonoBehaviour
+{
+    private Socket serverSocket;
+
+    #region AcceptClient
+
+    private bool isRunning = true;
+    
+    #endregion
+    void Start()
+    {
+        InitailSocket();
+    }
+    private List<SocketState> socketArr;
+    public void InitailSocket()
+    {
+        IPEndPoint endPoint=new IPEndPoint(IPAddress.Any,18010);
+        serverSocket=new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+        serverSocket.Bind(endPoint);
+        serverSocket.Listen(100);
+
+        socketArr=new List<SocketState>();
+        Thread tmpThread=new Thread(ListenRecv);
+        tmpThread.Start();
+    }
+    private void ListenRecv()
+    {
+        while (isRunning)
+        {
+            try
+            {
+                serverSocket.BeginAccept(new AsyncCallback(AsyncAcceptCallBack), serverSocket);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            Thread.Sleep(1000);
+        }
+    }
+    private void AsyncAcceptCallBack(IAsyncResult ar)
+    {
+        Socket listener = (Socket)ar.AsyncState;
+        Socket clientSocket = listener.EndAccept(ar);
+        SocketState socketState = new SocketState(clientSocket);
+        socketArr.Add(socketState);
+    }
+    void OnApplicationQuit()
+    {
+        serverSocket.Shutdown(SocketShutdown.Both);
+        serverSocket.Close();
+    }
+
+    void Update()
+    {
+        if (socketArr.Count>0)
+        {
+            for (int i = 0; i < socketArr.Count; i++)
+            {
+                socketArr[i].BeginRecv();
+            }
+        }
+    }
+}
